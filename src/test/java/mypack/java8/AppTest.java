702b587ffdf4b2218e94b96b85d070a64b1f6391
Atul Chauhan <atul.chauhan@publicissapient.com>
2020-07-07 17:23:45 +0530

    Updating pom file

---
+++ src/test/java/mypack/java8/AppTest.java
@@ -0,0 +1,16 @@
+package mypack.java8;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+	@Test
+	@BeforeAll
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
